713d8b858ba6112446d70fd4142a82396ebc3b2e
Hélio Rocha <hjrocha@Helios-MacBook-Pro.local>
2019-07-30 14:51:46 +0100

    add test

---
+++ src/main/java/com/javademo/resources/Test.java
@@ -0,0 +1,31 @@
+package com.javademo.resources;
+
+import com.google.common.base.Optional;
+import com.codahale.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Path("/test")
+@Produces(MediaType.APPLICATION_JSON)
+public class Test {
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+
+    public Test(String template, String defaultName) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = new AtomicLong();
+    }
+
+    @GET
+    @Timed
+    public void sayHello(@QueryParam("name") Optional<String> name) {
+        
+    }
+}
